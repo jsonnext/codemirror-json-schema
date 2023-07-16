f8981ce6de614fca08ab4704b9cf9f649d89c3e2
Samuel Imolorhe <samuelimolo4real@gmail.com>
2023-07-16 10:09:00 +0200

    added json completion

---
+++ dev/sample-text.ts
@@ -0,0 +1,5 @@
+export const jsonText = `{
+  "name": "lexunicon",
+  "version": "0.0.0",
+  "description": "A lexicon for the unicon programming language",
+)`;
---
+++ src/debug.ts
@@ -0,0 +1,5 @@
+export const debug = {
+  log: (...args: any[]) => {
+    console.log(...args);
+  }
+}
