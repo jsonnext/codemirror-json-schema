7de1494a1898455e4fbbcade88368f99b58e00ac
Rikki Schulte <rikki.schulte@gmail.com>
2023-07-19 12:05:37 +0200

    docs/demo fixup

--- dev/sample-text.ts
+++ dev/sample-text.ts
@@ -2,6 +2,11 @@ export const jsonText = `{
   "name": "lexunicon",
   "version": "0.0.0",
   "description": "A lexicon for the unicon programming language",
+  "contributors": [{
+    "email": "email",
+    "url": "h"
+  }],
+  "type":
 }`;
 export const json5Text = `{
   "name": "lexunicon",
