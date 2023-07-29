8ccdea7633a8dba1c3ec41eb12fa24893c465184
Rikki Schulte <rikki.schulte@gmail.com>
2023-07-29 18:02:20 +0200

    start on basic tests for completion

--- src/__tests__/json-completion.spec.ts
+++ src/__tests__/json-completion.spec.ts
@@ -1,48 +1,75 @@
-import { it, describe, expect } from "vitest";
-import { JSONCompletion } from "../json-completion";
-import { CompletionResult, autocompletion } from "@codemirror/autocomplete";
-import { testSchema } from "./__fixtures__/schemas";
-import { EditorView } from "@codemirror/view";
+import { describe, it, expect } from "vitest";
+
 import { json, jsonLanguage } from "@codemirror/lang-json";
 import { EditorState } from "@codemirror/state";
+import {
+  CompletionContext,
+  CompletionResult,
+  CompletionSource,
+} from "@codemirror/autocomplete";
+import { jsonCompletion } from "../json-completion";
+import { JSONSchema7 } from "json-schema";
+import { testSchema2 } from "./__fixtures__/schemas";
 
-describe("json-completion", () => {
+async function expectCompletion(
+  doc: string,
+  results: CompletionResult["options"],
+  schema?: JSONSchema7,
+  conf: { explicit?: boolean } = {}
+) {
+  let cur = doc.indexOf("|"),
+    currentSchema = schema || testSchema2;
+  doc = doc.slice(0, cur) + doc.slice(cur + 1);
+  let state = EditorState.create({
+    doc,
+    selection: { anchor: cur },
+    extensions: [
+      json(),
+      jsonLanguage.data.of({
+        autocomplete: jsonCompletion(currentSchema),
+      }),
+    ],
+  });
+  let result = await state.languageDataAt<CompletionSource>(
+    "autocomplete",
+    cur
+  )[0](new CompletionContext(state, cur, !!conf.explicit));
+  if (!result) {
+    return expect(result).toEqual(results);
+  }
+
+  const filteredResults = result.options.map(
+    ({ detail, info, label, type }) => ({ detail, info, label, type })
+  );
+  expect(filteredResults).toEqual(results);
+}
+
+describe("jsonCompletion", () => {
+  console.log("hello");
   it("should return completion data for simple types", async () => {
-    expect(true).toEqual(true);
+    await expectCompletion('{ "f| }', [
+      {
+        label: "foo",
+        type: "property",
+        detail: "string",
+        info: "",
+      },
+    ]);
+  });
+  it("should return completion data for simple types", async () => {
+    await expectCompletion('{ "one| }', [
+      {
+        label: "oneOfEg",
+        type: "property",
+        detail: "",
+        info: "an example oneOf",
+      },
+      {
+        label: "oneOfEg2",
+        type: "property",
+        detail: "",
+        info: "",
+      },
+    ]);
   });
 });
-
-// const getCompletionResult = (
-//   jsonString: string,
-//   pos: number
-// ): Promise<CompletionResult | undefined> => {
-//   return new Promise((resolve, reject) => {
-//     console.log("hello1");
-//     const completionInstance = new JSONCompletion(testSchema);
-//     const state = EditorState.create({
-//       doc: jsonString,
-//       selection: { anchor: pos, head: pos },
-//       extensions: [
-//         json(),
-//         jsonLanguage.data.of({
-//           autocomplete: () => {
-//             const result =
-//               completionInstance.doComplete.bind(completionInstance);
-//               console.log(result)
-//             resolve(result);
-//             return result;
-//           },
-//         }),
-//       ],
-//     });
-//   });
-// };
-
-// describe("json-completion", () => {
-//   console.log("hello");
-//   it("should return completion data for simple types", async () => {
-//     const completionResult = await getCompletionResult('{ "f }', 5);
-//     console.log(completionResult);
-//     expect(completionResult).toEqual(true);
-//   });
-// });
