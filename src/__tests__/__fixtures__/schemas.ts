5fcf5ad9ba2472095105ad0537f3e21446df88bb
Rikki Schulte <rikki.schulte@gmail.com>
2023-07-30 12:14:42 +0200

    add more completion tests

--- src/__tests__/__fixtures__/schemas.ts
+++ src/__tests__/__fixtures__/schemas.ts
@@ -35,7 +35,43 @@ export const testSchema2 = {
     oneOfEg2: {
       oneOf: [{ type: "string" }, { type: "array" }],
     },
+    oneOfObject: {
+      oneOf: [
+        { $ref: "#/definitions/fancyObject" },
+        { $ref: "#/definitions/fancyObject2" },
+      ],
+    },
+    enum1: {
+      description: "an example enum with default bar",
+      enum: ["foo", "bar"],
+      default: "bar",
+    },
+    enum2: {
+      description: "an example enum without default",
+      enum: ["foo", "bar"],
+    },
+    booleanWithDefault: {
+      description: "an example boolean with default",
+      default: true,
+      type: "boolean",
+    },
   },
   required: ["foo", "object.foo"],
   additionalProperties: false,
+  definitions: {
+    fancyObject: {
+      type: "object",
+      properties: {
+        foo: { type: "string" },
+        bar: { type: "number" },
+      },
+    },
+    fancyObject2: {
+      type: "object",
+      properties: {
+        apple: { type: "string" },
+        banana: { type: "number" },
+      },
+    },
+  },
 } as JSONSchema7;
