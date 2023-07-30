c1290503d38f0400ff3d1d22e37590c44d5e62e1
Rikki Schulte <rikki.schulte@gmail.com>
2023-07-30 12:14:42 +0200

    add more completion tests

--- src/__tests__/json-completion.spec.ts
+++ src/__tests__/json-completion.spec.ts
@@ -30,6 +30,137 @@ describe("jsonCompletion", () => {
         info: "",
         template: '"oneOfEg2": ',
       },
+      {
+        detail: "",
+        info: "",
+        label: "oneOfObject",
+        template: '"oneOfObject": ',
+        type: "property",
+      },
+    ]);
+  });
+  it("should include defaults for enum when available", async () => {
+    await expectCompletion('{ "en| }', [
+      {
+        label: "enum1",
+        type: "property",
+        detail: "",
+        info: "an example enum with default bar",
+        // TODO: should this not autocomplete to default "bar"?
+        // template: '"enum1": "${bar}"',
+        template: '"enum1": #{}',
+      },
+      {
+        label: "enum2",
+        type: "property",
+        detail: "",
+        info: "an example enum without default",
+        template: '"enum2": #{}',
+      },
+    ]);
+  });
+  it("should include value completions for enum", async () => {
+    await expectCompletion('{ "enum1": "|" }', [
+      {
+        label: '"foo"',
+        info: "an example enum with default bar",
+      },
+      {
+        label: '"bar"',
+        detail: "Default value",
+      },
+    ]);
+  });
+  it("should include defaults for boolean when available", async () => {
+    await expectCompletion('{ "booleanW| }', [
+      {
+        type: "property",
+        detail: "boolean",
+        info: "an example boolean with default",
+        label: "booleanWithDefault",
+        template: '"booleanWithDefault": ${true}',
+      },
+    ]);
+  });
+  // TODO: should provide true/false completions
+  it("should include value completions for boolean", async () => {
+    await expectCompletion('{ "booleanWithDefault": | }', []);
+  });
+  it("should include insert text for objects", async () => {
+    await expectCompletion('{ "ob| }', [
+      {
+        type: "property",
+        detail: "object",
+        info: "",
+        label: "object",
+        template: '"object": {#{}}',
+      },
+    ]);
+  });
+  // this has regressed for json4 only for some reason
+  it("should include insert text for nested object properties", async () => {
+    await expectCompletion('{ "object": { "|" } }', [
+      {
+        detail: "string",
+        info: "an elegant string",
+        label: "foo",
+        template: '"foo": "#{}"',
+        type: "property",
+      },
+    ]);
+  });
+  it("should include insert text for nested object properties with filter", async () => {
+    await expectCompletion('{ "object": { "f|" } }', [
+      {
+        detail: "string",
+        info: "an elegant string",
+        label: "foo",
+        template: '"foo": "#{}"',
+        type: "property",
+      },
+    ]);
+  });
+  it("should autocomplete for oneOf with nested definitions and filter", async () => {
+    await expectCompletion('{ "oneOfObject": { "f|" } }', [
+      {
+        detail: "string",
+        info: "",
+        label: "foo",
+        template: '"foo": "#{}"',
+        type: "property",
+      },
+    ]);
+  });
+  it("should autocomplete for oneOf with nested definitions", async () => {
+    await expectCompletion('{ "oneOfObject": { "|" } }', [
+      {
+        detail: "string",
+        info: "",
+        label: "foo",
+        template: '"foo": "#{}"',
+        type: "property",
+      },
+      {
+        detail: "number",
+        info: "",
+        label: "bar",
+        template: '"bar": #{0}',
+        type: "property",
+      },
+      {
+        detail: "string",
+        info: "",
+        label: "apple",
+        template: '"apple": "#{}"',
+        type: "property",
+      },
+      {
+        detail: "number",
+        info: "",
+        label: "banana",
+        template: '"banana": #{0}',
+        type: "property",
+      },
     ]);
   });
 });
