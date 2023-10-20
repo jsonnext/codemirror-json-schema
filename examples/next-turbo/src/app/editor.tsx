69d94d33112d038bbafb11b88b4ff84c19be550c
Rikki Schulte <rikki.schulte@gmail.com>
2023-10-20 23:42:07 +0200

    poorly working example with next and turbo

---
+++ examples/next-turbo/src/app/editor.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { EditorState } from "@codemirror/state";
+import { useEffect, useRef } from "react";
+
+import { gutter, EditorView, lineNumbers } from "@codemirror/view";
+import { basicSetup } from "codemirror";
+import { history } from "@codemirror/commands";
+import { autocompletion, closeBrackets } from "@codemirror/autocomplete";
+import { lintGutter } from "@codemirror/lint";
+import { bracketMatching, syntaxHighlighting } from "@codemirror/language";
+import { oneDarkHighlightStyle, oneDark } from "@codemirror/theme-one-dark";
+import { JSONSchema7 } from "json-schema";
+
+// sample data
+import { jsonText, json5Text } from "../../../../dev/sample-text";
+import packageJsonSchema from "../../../../dev/package.schema.json";
+
+// json4
+import { jsonSchema } from "../../../../dist";
+
+console.log({ jsonSchema });
+
+const schema = packageJsonSchema as JSONSchema7;
+
+/**
+ * none of these are required for json4 or 5
+ * but they will improve the DX
+ */
+const commonExtensions = [
+  gutter({ class: "CodeMirror-lint-markers" }),
+  bracketMatching(),
+  basicSetup,
+  closeBrackets(),
+  history(),
+  autocompletion(),
+  lineNumbers(),
+  lintGutter(),
+  oneDark,
+  EditorView.lineWrapping,
+  EditorState.tabSize.of(2),
+  syntaxHighlighting(oneDarkHighlightStyle),
+];
+
+export const Editor = () => {
+  const editorRef = useRef<HTMLDivElement>(null);
+  const state = EditorState.create({
+    doc: jsonText,
+    extensions: [commonExtensions, jsonSchema(schema)],
+  });
+
+  useEffect(() => {
+    if (editorRef.current) {
+      new EditorView({
+        state,
+        parent: editorRef.current!,
+      });
+    }
+
+    return () => {
+      // editorRef.current?.remove();
+    };
+  }, [editorRef.current]);
+  return <div ref={editorRef}></div>;
+};
---
+++ examples/next-turbo/src/app/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import dynamic from "next/dynamic";
+
+const Editor = dynamic(async () => (await import("./editor")).Editor, {
+  ssr: false,
+});
+
+/**
+ * json4!
+ */
+
+/**
+ * json5!
+ */
+
+// Hot Module Replacement
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Editor />
+    </main>
+  );
+}
