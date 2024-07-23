de745f1bd3b32eeedaad0838cd94fcb45972c3d0
SN <6432132+samnoyes@users.noreply.github.com>
2024-07-23 13:33:29 -0700

    remove everything shiki

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,43 +1,4 @@
-import md from "markdown-it";
-import shiki from "@shikijs/markdown-it";
-
-// const defaultPlugins = [
-//   "markdown-it-abbr",
-//   "markdown-it-deflist",
-//   "markdown-it-emoji",
-//   "markdown-it-footnote",
-//   "markdown-it-ins",
-//   "markdown-it-mark",
-//   "markdown-it-sub",
-//   "markdown-it-sup",
-//   "markdown-it-task-lists",
-//   "markdown-it-toc",
-//   "markdown-it-attrs",
-//   "markdown-it-katex",
-//   "markdown-it-external-links",
-//   "markdown-it-table-of-contents",
-//   "markdown-it-anchor",
-//   "markdown-it-implicit-figures",
-//   "markdown-it-video",
-//   "markdown-it-highlightjs",
-// ];
-
-const renderer = md({
-  linkify: true,
-  typographer: true,
-});
-
-(async () => {
-  const shikiRenderer = await shiki({
-    themes: {
-      light: "vitesse-light",
-      dark: "vitesse-dark",
-    },
-  });
-  renderer.use(shikiRenderer);
-})();
-
 export function renderMarkdown(markdown: string, inline: boolean = true) {
-  if (!inline) return renderer.render(markdown);
-  return renderer.renderInline(markdown);
+  if (!inline) markdown;
+  return markdown;
 }
